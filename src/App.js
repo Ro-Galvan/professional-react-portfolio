20ec7049168500c3d12d6f812a1684ad8a686626
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-01-06 16:18:13 -0500

    imported all the components to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 // type rafce to get app imort and export template 
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
