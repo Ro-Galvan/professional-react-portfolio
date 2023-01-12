c3052590362723807f78d8587d97e0eeede338bf
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-01-12 17:45:02 -0500

    started the active or not feature on nav bar but will save and finish that part another day

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,11 +5,15 @@ import {FaUser} from 'react-icons/fa'
 // import {GiBrain} from 'react-icons/gi'
 import {FaBookOpen} from 'react-icons/fa'
 // import {MdEmail} from 'react-icons/md'
+// use useState if you want to implement active feature on nav bar-- 1:18
+import { useState } from 'react'
 
 export default function Nav() {
+  // didn't finish the active or not feature
+  // const [activeNav, setActiveNav] = useState ('#');
   return (
     <nav>
-      <a href="#"><AiFillHome/></a>
+      <a href="#" className='active'><AiFillHome/></a>
       <a href="#about"><FaUser/></a>
       {/* <a href="#skills"><GiBrain/></a> */}
       <a href="#portfolio"><FaBookOpen/></a>
