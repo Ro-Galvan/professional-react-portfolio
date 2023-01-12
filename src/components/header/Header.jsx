999174f9446aacb18ef4c9e3e6eed72b2b8cff86
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-01-12 15:10:55 -0500

    added picture and more files to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    // using sementaic tags 
+    <header>
+      {/* Used BEM naming convention of Block, Element, Modifier  */}
+      <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Rocio Galvan</h1>
+      <h5 className="text-light">Full-stack Developer</h5>
+      <CTA />
+      <div className="me">
+        <img src={ME} alt="pictureOfRocio" />
+      </div>
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeaderSocials() {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/rocio-galvan/" target="blank"></a>
+      <a href="https://github.com/Ro-Galvan" target="_blank"></a>
+
+    </div>
+  )
+}
