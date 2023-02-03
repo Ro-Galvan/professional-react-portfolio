4ba8056c23d6fd06f2f492d34a7e628be5d82083
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-02-03 16:23:09 -0500

    had to comment out the height 100vh in index.css so the footer would move to the bottom of page and there wouldnt be such a huge gap between sections. Also made some small changes on my picture

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './footer.css'
+import {ImLinkedin} from 'react-icons/im'
+import {ImGithub} from 'react-icons/im'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>ROCIO</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+      <a href="https://www.linkedin.com/in/rocio-galvan/" target="blank" rel="noreferrer"><ImLinkedin/></a>
+      <a href="https://github.com/Ro-Galvan" target="_blank" rel="noreferrer"><ImGithub/></a>
+      </div>
+      {/* <div className="footer__copyright">
+
+      </div> */}
+    </footer>
   )
 }
