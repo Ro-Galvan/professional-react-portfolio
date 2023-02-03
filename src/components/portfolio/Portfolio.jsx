b29a36937eb3abf5f6c0c1bbf22b5f5f6ab89352
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-02-03 10:59:26 -0500

    started on project section and stubbed out how each project will look

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/regex.jpg'
 
 export default function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {/* Project 1 */}
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <img src={IMG1} alt="regex tutorial" />
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' target="_blank" rel="noreferrer">Github</a>
+          <a href="http://TBD.com" className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+         {/* Project 2 */}
+         <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <img src={IMG1} alt="regex tutorial" />
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' target="_blank" rel="noreferrer">Github</a>
+          <a href="http://TBD.com" className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+         {/* Project 3 */}
+         <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <img src={IMG1} alt="regex tutorial" />
+          <h3>This is a portfolio item title</h3>
+          <a href="http://github.com" className='btn' target="_blank" rel="noreferrer">Github</a>
+          <a href="http://TBD.com" className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
